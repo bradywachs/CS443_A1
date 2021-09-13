5be55e18ccb1f1f9b39b9768394526faf155ee55
brady wachs <bmwachs@crimson.ua.edu>
2021-09-12 22:10:30 -0500

    working on column3

--- hexdump/hexdump.py
+++ hexdump/hexdump.py
@@ -1,5 +1,6 @@
 import argparse
 import binascii
+from textwrap import wrap
 
 
 def main():
@@ -9,12 +10,29 @@ def main():
     args = parser.parse_args()
     file = args.filename
 
-    # try to open file
+    # integer representation of offset
+    n = 0
+
+    # logic contained in try block
     try:
         with open(file, "rb") as f:
+            # iterate over every 16 byte chunk in file
             for chunk in iter(lambda: f.read(16), b''):
                 hex_line = binascii.hexlify(chunk)
-                print(hex_line)
+                hex_line = str(hex_line)
+                col2 = " ".join(hex_line[i:i+2] for i in range(0, len(hex_line), 2))    # column2 is the byte values in hex
+                col2 = f'{col2[:23]} {col2[23:]}'                                       # add extra space inbetween 8th and 9th byte
+                # print(col2)
+
+                col3 = ""
+                for i in hex_line:
+                    # range of printable ascii characters [32,127] - range funct goes to stop - 1 value
+                    if i in range(32,128):
+                        col3 = "".join(chr(i))
+                    else:
+                        col3 = "".join(".")
+                # increment int representation of offset
+                n += 1
     
     except Exception as ex:
         print(f"ERROR - Exception: {ex}")
