aeb7430bf905a168ca2f22a9d757eeadc9238950
brady wachs <bmwachs@crimson.ua.edu>
2021-09-12 20:33:02 -0500

    parse args and print hex
    trying simple approach of cencoding entire file to hex at once and then just printing that

---
+++ hexdump/hexdump.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+def main():
+    # get filename
+    parser = argparse.ArgumentParser(description="Python implementation of hexdump tool")
+    parser.add_argument("filename", type=str, help="Name of file that hexdump tool will be applied to")
+    args = parser.parse_args()
+    file = args.filename
+
+    # try to open file
+    try:
+        with open(file, "rb") as f:
+            hex_file = f.read().encode('hex')
+            print(hex_file)
+    except Exception as ex:
+        print(f"ERROR - Exception: {ex}")
+
+if __name__ == "__main__":
+    main()
